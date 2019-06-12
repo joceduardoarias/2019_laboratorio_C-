8293e94cb588b4f65bb8a73622628128b55fef62
alumno <alumno>
2019-06-12 11:53:45 -0300

    base de datos abm

---
+++ BaseDatos/FrmListado/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ClassLibrary;
+namespace FrmListado
+{
+    public partial class Form1 : Form
+    {
+        private List<Persona> personas;
+        private DataTable table;
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            AccesoDatos accesoDatos = new AccesoDatos();
+            dataGridView1.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            personas = accesoDatos.TraerTodos();//cargo la lista
+            table = accesoDatos.TablaPersona();//cargo la tabla
+            dataGridView1.DataSource = personas; //le paso la lista de personas cargadas
+        }
+    }
+}
---
+++ BaseDatos/VistaConsola/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClassLibrary;
+using System.Data;
+namespace VistaConsola
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+            //List<Persona> lista = new List<Persona>();
+            //Persona persona = new Persona(52, "evo", "morales", 100);
+            AccesoDatos datos = new AccesoDatos();
+            //// datos.AgregarPersona(persona);
+            //datos.ModificarPersona(persona);
+            //lista = datos.TraerTodos();
+            //foreach (Persona item in lista)
+            //{
+            //    Console.WriteLine(item.ToString());
+            //}
+            //uso del metodo datatable
+            DataTable table = new DataTable("Personas");
+            // table = datos.TablaPersona();
+            //foreach (DataRow item in table.Rows)
+            //{
+            //    Console.WriteLine(item[0].ToString(), item["id"].ToString(),item["nombre"].ToString(), item["apellido"], item["edad"].ToString());
+            //}
+
+            //table.WriteXml 
+            table.WriteXmlSchema("persona_esquema.xml"); //serializar
+            table.WriteXml("personas_datos.xml");//serializar
+
+            //table.ReadXmlSchema("persona_esquema.xml");//deserealizar
+            //table.ReadXml("personas_datos.xml");//deserealizar
+            Console.ReadKey();
+        }
+    }
+}
